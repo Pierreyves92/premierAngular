7e18c994fe73bcac2abd29201fb4942c5e0fdeca
PierreYves92 <pierreyves92@gmail.com>
2018-07-15 11:27:03 +0200

    commit 1

---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {AppareilService} from '../services/appareil.service';
+
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+
+  @Input() appareilName: string;
+  @Input() appareilStatus: string;
+  @Input() indexOfAppareil: number;
+  @Input() id:number;
+
+
+  constructor(private appareilService: AppareilService) { }
+
+  ngOnInit() {
+  }
+
+  getStatus ()
+  {
+    return this.appareilStatus ;
+  }
+
+  getColor ()
+  {
+    if this.appareilStatus === "allumé" {return 'green'} ;
+    else return 'red' ;
+  }
+
+  onSwitchOn ()
+  {
+    this.appareilService.swithOnone(this.indexOfAppareil);
+  }
+
+  onSwitchOff ()
+  {
+    this.appareilService.switchOffOne(this.indexOfAppareil);
+  }
+}
+
